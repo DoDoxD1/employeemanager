ea5126663a2ba92cd64a7d324592cebfd85270cd
= <=>
2025-05-10 11:35:52 +0530

    Initial commit

---
+++ src/main/java/com/arihant/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.arihant.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String s) {
+        super((s));
+    }
+}
